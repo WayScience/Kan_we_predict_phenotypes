64fc5e2040a4a996b7e017835858604c001e4f04
axiomcura <erikishere3@gmail.com>
2024-08-19 13:33:44 -0600

    added download module

---
+++ notebooks/0.download-data/0.download-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# activate conda env
+conda activate cell-injury
+
+# convert notebooks into python scripts
+jupyter nbconvert --to python --output-dir=nbconverted/ *.ipynb
+
+# run the scripts
+python nbconverted/0.download-data.py
