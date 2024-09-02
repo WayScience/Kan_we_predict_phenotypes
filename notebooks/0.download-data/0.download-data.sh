c9dadf93c0e3bdc6c415cb329b0141ed85b2b198
axiomcura <erikishere3@gmail.com>
2024-09-02 12:44:07 -0600

    updated env name

--- notebooks/0.download-data/0.download-data.sh
+++ notebooks/0.download-data/0.download-data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # activate conda env
-conda activate kan
+conda activate cell-kan
 
 # convert notebooks into python scripts
 jupyter nbconvert --to script --output-dir=nbconverted/ *.ipynb
 
 # run the scripts
-conda run -n kan python nbconverted/0.download-data.py
+conda run -n cell-kan python nbconverted/0.download-data.py
